9be6161b526e9975a085a309162bdc4aac83169f
binarypirate <aliadi@gmail.com>
2021-09-14 01:04:17 -0700

    COmplete

---
+++ README.md
@@ -0,0 +1,30 @@
+# SteganographyAndroid
+An android app to hide files within other files.
+
+Select a base image and a secret image to hide within that base image. The app will give you an image file that looks and behaves like the base image would. Send this file to someone you want to share the secret image with. They then run the file through the apps decoder to retrieve the secret image. To anyone else, the file just looks like the base image.
+
+Initial Screen
+
+![Initial Screen](./readme_res/Screenshot_2015-10-18-09-45-48.png)
+
+Encoding Screen
+
+![Encoding Screen1](./readme_res/Screenshot_2015-10-18-09-45-55.png)
+
+![Encoding Screen2](./readme_res/Screenshot_2015-10-18-09-46-23.png)
+
+Encoded Image
+
+![Encoded Image](./readme_res/Screenshot_2015-10-18-09-46-39.png)
+
+Back to the Initial Screen
+
+![Initial Screen](./readme_res/Screenshot_2015-10-18-09-45-48.png)
+
+Decoding Screen
+
+![Decoding Screen](./readme_res/Screenshot_2015-10-18-09-47-02.png)
+
+Pepe Retrieved!
+
+![Retrieval Screen](./readme_res/Screenshot_2015-10-18-09-47-10.png)
---
+++ app/src/main/java/com/goobers/steganography/ImageActivity.java
@@ -0,0 +1,71 @@
+package com.goobers.steganography;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+public class ImageActivity extends Activity {
+
+    private static final String LOG_TAG = ImageActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        Intent intent = getIntent();
+        String displayPath = intent.getStringExtra(EncodeActivity.EXTRA_FILE_TAG);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Bitmap bitmap = BitmapFactory.decodeFile(displayPath);
+        imageView.setImageBitmap(bitmap);
+        Calendar calendar = Calendar.getInstance();
+        String name = "" + calendar.get(Calendar.YEAR) + calendar.get(Calendar.MONTH) + calendar.get
+                (Calendar.DATE) + calendar.get(Calendar.HOUR) + calendar.get(Calendar.MINUTE)
+                + calendar.get(Calendar.SECOND);
+        File tempFile = new File(Environment.getExternalStorageDirectory().getPath() +
+                "/Pictures/Screenshots/" + name + ".png");
+        try {
+            tempFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
